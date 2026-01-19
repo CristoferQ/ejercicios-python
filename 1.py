326886f7c3be3b1f38d6a4b3856b5f2c7a5e2c26
Cristofer Quiroz <ceql@live.cl>
2026-01-18 23:11:13 -0300

    Se sube primer ejercicio

---
+++ 1.py
@@ -0,0 +1,18 @@
+# 🟢 Ejercicio 1: Suma de pares
+# 📌 Descripción
+# Dado un número entero N, imprime la suma de todos los números pares desde 1 hasta N (inclusive).
+# Ejemplos
+# Entrada 10 Salida 30
+# Entrada 1 Salida 0
+# Entrada 8 Salida 20
+
+def sumaPares(n):
+    sumaTotal = 0
+    for i in range(1,n+1):
+        if(i%2==0):
+            sumaTotal += i
+    return sumaTotal
+
+print(sumaPares(10))
+print(sumaPares(1))
+print(sumaPares(8))
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ejercicios-python
+
+ejercicios para python con descripcion y 3 pruebas de entrada y salida
+
+1. Suma de pares
+
+
